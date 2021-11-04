516c793b92f52189a01c0b09638e0d887cfcd09e
Madison Shimizu <mhshimizu@outlook.com>
2021-11-03 15:52:31 -1000

    commit tree df0a079225f85153a1d6f6a339876a6799273a60

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+
+// Initial solution:
+    // const returnFirstTwoDrivers = function (drivers = ['Sally', 'Bob', 'Freddy', 'Claudia']) {
+    //     return drivers.slice (0, 2);
+    // }
+// Simplified:
+    const returnFirstTwoDrivers = drivers => drivers.slice (0, 2);
+
+// Initial solution:
+    // const returnLastTwoDrivers = function (drivers = ['Sally', 'Bob', 'Freddy', 'Claudia']) {
+    //     return drivers.slice (-2);
+    // }
+// Simplified:
+    const returnLastTwoDrivers = drivers => drivers.slice (-2);
+
+// Next solution:
+    const selectingDrivers = [returnFirstTwoDrivers,  returnLastTwoDrivers];
+
+// Initial solution:
+    // function createFareMultiplier (multiplier) {
+    //     return rideFare => rideFare * multiplier;
+    //     }
+// Simplified:
+    const createFareMultiplier = multiplier => rideFare => rideFare * multiplier;
+
+// Next solution:
+const fareDoubler = createFareMultiplier (2);
+
+// Next solution:
+const fareTripler = createFareMultiplier (3);
+
+// Initial solution:
+    // function selectDifferentDrivers (array, fn) {
+    //     return fn(array);
+    // }
+// Simplified:
+const selectDifferentDrivers = (array, fn) => fn (array);
+
+
